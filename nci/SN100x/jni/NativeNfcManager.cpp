9d5b9fe9d0ed0fa909e1c177ad50002b3f4ecf48
nxf31698 <suryaprakash.konduru@nxp.com>
2022-08-04 16:41:08 +0530

    Added support for handling TxLDO overcurrent notification.
    Restarting the RF Discovery recovers the RF when it receives TxLDO
    overcurrent notification.
    
    (cherry picked from commit f5097ab717364e32acc200e32a9349bdf9c7469d)
    Change-Id: Ibe3e238b70c4c53f1f584e79be3944772237ca4f
    (cherry picked from commit 7bc5156a38fc1e77f957e1a6015d27e1f82b6d66)

--- nci/SN100x/jni/NativeNfcManager.cpp
+++ nci/SN100x/jni/NativeNfcManager.cpp
@@ -186,6 +186,7 @@ jmethodID gCachedNfcManagerNotifyHwErrorReported;
 #if(NXP_EXTNS == TRUE)
 jmethodID gCachedNfcManagerNotifyLxDebugInfo;
 jmethodID gCachedNfcManagerNotifyTagAbortListeners;
+jmethodID gCachedNfcManagerNotifyCoreGenericError;
 #endif
 
 const char* gNativeP2pDeviceClassName =
@@ -320,6 +321,7 @@ static int nfcManager_staticDualUicc_Precondition(int uiccSlot);
 static int nfcManager_setPreferredSimSlot(JNIEnv* e, jobject o, jint uiccSlot);
 static bool nfcManager_deactivateOnPollDisabled(tNFA_ACTIVATED& activated);
 static jint nfcManager_enableDebugNtf(JNIEnv* e, jobject o, jbyte fieldValue);
+static void nfcManager_restartRFDiscovery(JNIEnv* e, jobject o);
 #endif
 static uint16_t sCurrentConfigLen;
 static uint8_t sConfig[256];
@@ -1022,6 +1024,8 @@ static jboolean nfcManager_initNativeStruc(JNIEnv* e, jobject o) {
       e->GetMethodID(cls.get(),"notifySeInitialized", "()V");
   gCachedNfcManagerNotifyTagAbortListeners =
       e->GetMethodID(cls.get(), "notifyTagAbort", "()V");
+  gCachedNfcManagerNotifyCoreGenericError =
+      e->GetMethodID(cls.get(), "notifyCoreGenericError", "(I)V");
 #endif
   gCachedNfcManagerNotifyTransactionListeners = e->GetMethodID(
       cls.get(), "notifyTransactionListeners", "([B[BLjava/lang/String;)V");
@@ -1221,6 +1225,21 @@ if (!sP2pActive && eventData->rf_field.status == NFA_STATUS_OK) {
       SyncEventGuard guard(sNfaSetPowerSubState);
       sNfaSetPowerSubState.notifyOne();
     } break;
+#if(NXP_EXTNS == TRUE)
+    case NFA_DM_GEN_ERROR_REVT: {
+      struct nfc_jni_native_data* nat = getNative(NULL, NULL);
+      JNIEnv* e = NULL;
+      ScopedAttach attach(nat->vm, &e);
+      if (e == NULL) {
+        LOG(ERROR) << StringPrintf("jni env is null");
+        return;
+      }
+
+      e->CallVoidMethod(nat->manager,
+                        android::gCachedNfcManagerNotifyCoreGenericError,
+                        eventData->status);
+    } break;
+#endif
     default:
       DLOG_IF(INFO, nfc_debug_enabled)
           << StringPrintf("%s: unhandled event", __func__);
@@ -3352,6 +3371,7 @@ static JNINativeMethod gMethods[] = {
     {"setPreferredSimSlot", "(I)I", (void*)nfcManager_setPreferredSimSlot},
     {"doNfcSelfTest", "(I)I", (void*) nfcManager_nfcSelfTest},
     {"doEnableDebugNtf", "(B)I", (void*) nfcManager_enableDebugNtf},
+    {"doRestartRFDiscovery", "()V", (void*)nfcManager_restartRFDiscovery},
 #endif
     {"doSetNfcSecure", "(Z)Z", (void*)nfcManager_doSetNfcSecure},
     {"getNfaStorageDir", "()Ljava/lang/String;",
@@ -3743,6 +3763,22 @@ static jint nfcManager_getFwVersion(JNIEnv * e, jobject o) {
     DLOG_IF(INFO, nfc_debug_enabled)
         << StringPrintf("%s: exit; version =0x%X", __func__, version);
     return version;
+}
+
+/*******************************************************************************
+**
+** Function:        nfcManager_restartRFDiscovery
+** Description:     Restarts RF discovery
+**
+**                  e: JVM environment.
+**                  o: Java object.
+**
+*******************************************************************************/
+static void nfcManager_restartRFDiscovery(JNIEnv*, jobject) {
+  if (sRfEnabled) {
+    android::startRfDiscovery(false);
+  }
+  android::startRfDiscovery(true);
 }
 
   /*******************************************************************************
