9ef719a2cbb30afa7f55715a196b75ca99cf19f7
Amruth Naga <quic_amrunaga@quicinc.com>
2023-10-20 14:58:55 +0530

    NFC: ParcelableFileDescriptor replace FileDescriptor
    ParcelableFileDescriptor replace FileDescriptor
    cherry picked from Ic3e7f1360c6a7552865a1d726467aca711490ecd
    
    Change-Id: I8b86662066fc7fcc08c9f4e322ac7f9c49ab977a

--- src/com/android/nfc/cardemulation/RegisteredServicesCache.java
+++ src/com/android/nfc/cardemulation/RegisteredServicesCache.java
@@ -54,6 +54,7 @@ import android.nfc.cardemulation.ApduServiceInfo;
 import android.nfc.cardemulation.CardEmulation;
 import android.nfc.cardemulation.HostApduService;
 import android.nfc.cardemulation.OffHostApduService;
+import android.os.ParcelFileDescriptor;
 import android.os.UserHandle;
 import android.os.UserManager;
 import android.sysprop.NfcProperties;
@@ -408,7 +409,7 @@ public class RegisteredServicesCache {
                     continue;
                 } else {
                     for (AidGroup group : dynamicSettings.aidGroups.values()) {
-                        serviceInfo.setOrReplaceDynamicAidGroup(group);
+                        serviceInfo.setDynamicAidGroup(group);
                     }
                     if (dynamicSettings.offHostSE != null) {
                         serviceInfo.setOffHostSecureElement(dynamicSettings.offHostSE);
@@ -620,7 +621,7 @@ public class RegisteredServicesCache {
                 Log.e(TAG, "OffHostSE is not set");
                 return false;
             }
-            serviceInfo.unsetOffHostSecureElement();
+            serviceInfo.resetOffHostSecureElement();
             DynamicSettings dynSettings = services.dynamicSettings.get(componentName);
             if (dynSettings != null) {
               String offHostSE = dynSettings.offHostSE;
@@ -669,7 +670,7 @@ public class RegisteredServicesCache {
                     return false;
                 }
             }
-            serviceInfo.setOrReplaceDynamicAidGroup(aidGroup);
+            serviceInfo.setDynamicAidGroup(aidGroup);
             DynamicSettings dynSettings = services.dynamicSettings.get(componentName);
             if (dynSettings == null) {
                 dynSettings = new DynamicSettings(uid);
@@ -753,6 +754,12 @@ public class RegisteredServicesCache {
 
     public void dump(FileDescriptor fd, PrintWriter pw, String[] args) {
         pw.println("Registered HCE services for current user: ");
+        ParcelFileDescriptor pFd;
+        try {
+            pFd = ParcelFileDescriptor.dup(fd);
+        } catch (IOException e) {
+            return;
+        }
 
         synchronized (mLock) {
             for (UserHandle uh : mUserHandles) {
@@ -761,7 +768,7 @@ public class RegisteredServicesCache {
                 pw.println("User " + um.getUserName() + " : ");
                 UserServices userServices = findOrCreateUserLocked(uh.getIdentifier());
                 for (ApduServiceInfo service : userServices.services.values()) {
-                    service.dump(fd, pw, args);
+                    service.dump(pFd, pw, args);
                     pw.println("");
                 }
                 pw.println("");
